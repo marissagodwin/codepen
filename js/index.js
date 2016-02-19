fe77fed310fedd6f0e11bcdd24875da091984aca
marissagodwin <marissa.e.godwin@gmail.com>
2016-02-19 10:46:51 -0500

    feat(css,html): add new content, style
    Add html content. Add css styling
    
    Fix navigation issues

--- js/index.js
+++ js/index.js
@@ -2,4 +2,6 @@ $(".menu-toggle").on('click', function() {
   $(this).toggleClass("on");
   $('.menu-section').toggleClass("on");
   $("nav ul").toggleClass('hidden');
+  $("h1").toggleClass('hidden');
+
 });
